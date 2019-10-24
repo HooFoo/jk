a36c84a18357b3145a6e436d65686fcbb95d4099
Dmitrii Davydov <swember@gmail.com>
2019-10-24 18:48:08 +0300

    Install swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :address, only: [:index]
