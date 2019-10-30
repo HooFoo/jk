4c46823d4fed1483f4f0bdfea525f204c1b6b44f
Gennadiy Khudyakov <lHooFool@gmail.com>
2019-10-30 20:50:44 +0700

    Swagger test

--- spec/factories/advertisements.rb
+++ spec/factories/advertisements.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :advertisement do
     title { FFaker::Lorem.word }
     description { FFaker::Lorem.paragraph }
     price { 1 }
     currency { 'RUB' }
-    due_in { "2019-10-26 14:44:53" }
-    category { "other" }
+    due_in { '2019-10-26 14:44:53' }
+    category { 'other' }
   end
 end
---
+++ spec/integration/api/v1/buildings/advertisements_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+
+describe 'Advertisements API' do
+  let(:building) { create :building }
+  let(:user) { create :user }
+  let(:advertisement) { create :advertisement, building: building, user: user }
+  let(:building_id) { building.id }
+
+  path '/api/v1/buildings/{building_id}/advertisements/{id}' do
+    get 'Retrieves the advertisement by provided id' do
+      tags 'Advertisements'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer, required: true
+      parameter name: :building_id, in: :path, type: :integer, required: true
+
+      response '200', 'advertisement returned' do
+        schema type: :object,
+               properties: {
+                 data: {
+                   type: :object,
+                   properties: {
+                     id: {type: :string},
+                     type: {type: :string},
+                     attributes: {
+                       type: :object,
+                       title: {type: :string},
+                       full_address: {type: :string},
+                       description: {type: :number},
+                       price: {type: :number},
+                       currency: {type: :string},
+                       due_in: {type: :string},
+                       catrgory: {type: :string}
+                     }
+                   }
+                 }
+               }
+        let(:id) { advertisement.id }
+        run_test!
+      end
+    end
+  end
+end
