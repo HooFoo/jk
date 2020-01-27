85d0d09ce6dc4dada2bcce50386b7db925681758
Valeriy Golovko <nevalikne@gmail.com>
2020-01-27 16:06:10 +0200

    removed components

--- app/frontend/src/dipendency-injection/with-dependencies.tsx
+++ app/frontend/src/dipendency-injection/with-dependencies.tsx
@@ -1,27 +1,6 @@
-// import * as React from "react";
 import Container from "./container";
-
-// export default function withDependencies<T extends object>(Component: React.ComponentType<T>) {
-//   let resolve: any =
-//         (typeCtor: new (...args: any[]) => any) => Container.Instance.resolve(typeCtor);
-
-//   return class Injector extends React.ComponentType<T> {
-//     render() {
-//       return <Component
-//         {...this.state}
-//         {...this.props}
-//         {...resolve}
-//       />
-//     }
-//   }
-// }
-
 import * as React from 'react';
 import { ResolveDependencyProps } from './resolve-dependency-props';
-
-// export interface ResolveProps {
-//     additionalProp: string;
-// }
 
 export default function withDependencies<Props extends ResolveDependencyProps>(WrappedComponent: React.ComponentType<Props>) : React.ComponentType<Omit<Props, "resolve">> {
     let resolve: any =
