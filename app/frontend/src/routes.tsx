12dc098bd3fb4daec0aefb3f2ce66728fdfaccd7
Valeriy Golovko <nevalikne@gmail.com>
2020-02-03 15:02:42 +0200

    review fixes

--- app/frontend/src/routes.tsx
+++ app/frontend/src/routes.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import AddressSelectPage from './pages/address-select';
 import BuildingPage from './pages/building';
