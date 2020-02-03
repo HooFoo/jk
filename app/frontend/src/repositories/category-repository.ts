d361ffcea91176d8d76f3238f48430409e9e4299
Valeriy Golovko <nevalikne@gmail.com>
2020-02-03 14:50:47 +0200

    review fixes

--- app/frontend/src/repositories/category-repository.ts
+++ app/frontend/src/repositories/category-repository.ts
@@ -1,9 +1,14 @@
 import Category from "../models/category";
 import BaseRepository from "./base-repository";
 import { AxiosResponse } from "axios";
+import { Inject } from "../dependency-injection/inject.decorator";
 
 export default class CategoryRepository extends BaseRepository {
   private readonly baseUrl = '/categories';
+
+  @Inject
+  public inject(): void {
+  }
 
   public index(): Promise<Category[]> {
     return this.http.get(this.baseUrl)
